6d36ced06f944a682e1bcd89f431edc172040e5e
Dthen <65791582+Dthen@users.noreply.github.com>
2020-11-24 21:48:29 +0000

    Update greeter.js

--- greeter.js
+++ greeter.js
@@ -1,17 +1,25 @@
 const {greeting, rolesChannelName, welcomeChannelName, } = require('./config.json');
 module.exports = (member) => {
-	
+	console.log('running greeter.js')
+	console.log(member)
+
+
 	//Fetch channel IDs from names
 	const rolesChannel = member.guild.channels.cache.find(r => r.name === rolesChannelName);
 	const welcomeChannel= member.guild.channels.cache.find(r => r.name === welcomeChannelName);
+	console.log('fetching channels')
 
 	//Check the channel exists
-	if (!welcomeChannel || !rolesChannel) return;
+	if (!welcomeChannel || !rolesChannel) {
+		console.log('no Welcome channel')
+		return;
+	}
 	
 	//Prepare formatting for welcome message.
 	const greetingMessage = greeting
 		.replace(/\${member}/g, member)
 		.replace(/\${rolesChannelName}/g, rolesChannel);
+		console.log('greeting message formatted')
 
 	//Greet the new user
 	welcomeChannel.send(greetingMessage);
