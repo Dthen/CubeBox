491078b50d566e282811e20793a7c585e1f3d8c4
Dthen <65791582+Dthen@users.noreply.github.com>
2021-02-02 23:52:05 +0000

    Added teams comand
    Much thanks, Dave. Closes #46

---
+++ commands/teams.js
@@ -0,0 +1,45 @@
+module.exports = {
+	name: 'teams',
+	description: `Splits the users in the current voice channel into teams`,
+	cooldown: 5,
+	execute(message, args) {
+        //If no team number is specificed (no arguments, assumes 2 teams)
+        let teams = 2
+        if (args.length >= 1) teams = args[0]
+        
+        // Create a list of players in voice channel, ignoring deafened users and bots,
+        // then shuffle that list
+        const players = (message.member.voice.channel.members
+            .filter(member => !member.voice.selfDeaf || !member.user.bot))
+            .sort(() => Math.random() - 0.5)
+            
+        //Divide the players into teams  
+        let playersByTeams = []
+        while (playersByTeams.length < teams)
+        {
+            playersByTeams.push([])
+        } 
+
+        players.forEach((player, index) => {
+            playersByTeams[index % playersByTeams.length].push(player)
+        } )
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle (`Here is your list of teams:`)
+        .setColor(0x553E90)
+        .setDescription ()
+        .addFields(
+            ...playersInTeams.map((players, index) => {
+                return {
+                    name: `Team ${index + 1}`,
+                    value: players.join('\n'),
+                    inline: true,
+                };
+            })
+        )
+    // reply wih list of users in the same voice channel divided into teams.
+    message.channel.send(embed)
+    console.log('Replied with list of teams')
+
+	},
+};
