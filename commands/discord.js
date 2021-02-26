9998ba2946135ec721d3e9774816223a577cc9ce
Dthen <65791582+Dthen@users.noreply.github.com>
2021-02-26 01:58:27 +0000

    tidied up a couple wee commands

--- commands/discord.js
+++ commands/discord.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'discord',
 	description: `Links to the Discord server's invite page`,
 	cooldown: 5,
-	execute(message, args) {
+	execute(message) {
 		message.channel.send('https://discord.thepurplecubes.com/');
 	},
 };
--- commands/restart.js
+++ commands/restart.js
@@ -1,19 +1,17 @@
-const { modRoleName } = require('../config/moderation.json');
+const {modRoleId, adminRoleId} = require('../config/moderation.json');
 module.exports = {
 	name: 'restart',
 	description: 'Restarts the bot',
 	args: false,
 	cooldown: 60,
-	execute(message, args) {
-		//Get Mod role from mod role name
-		const modRole = message.guild.roles.cache.find(r => r.name === modRoleName);
+	execute(message, client) {
 		//Check the user has moderator permissions
-		if (!message.member.roles.cache.has(modRole.id)) {
+		if (!message.member.roles.cache.has(modRoleId || adminRoleId) ) {
 			return message.reply('You\'re not allowed to do that.')
 		}
 		message.reply('Restarting ...').then(() => {
-			log('Ready to exit...');
-			message.client.destroy();
+			log('Restart command issued, preparing to exit process');
+			client.destroy();
 			process.exit();
 		  });
 	}
