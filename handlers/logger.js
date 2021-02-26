aaec6ae1b75900bd9ab3052127ff0057387166d1
Dthen <65791582+Dthen@users.noreply.github.com>
2021-02-26 16:25:13 +0000

    Update logger.js

--- handlers/logger.js
+++ handlers/logger.js
@@ -6,7 +6,7 @@ const adminRoleId = require('../config/moderation.json')
 module.exports = (guild, error) => {
     if (loggerOn) {
         let timestamp = new Date().toUTCString();
-        if (logToConsole) log(timestamp + ': ' + error);
+        if (logToConsole) console.log(timestamp + ': ' + error);
         if (logToFile) fs.appendFileSync('log.txt', timestamp + ': ' + error);
         if (logToChannel) getChannelById(guild, logChannelID).reply(timestamp + ': ' + error);
         if (logToAdmins) guild.roles.cache.fetch(adminRoleId).send(timestamp + ': ' + error);        
